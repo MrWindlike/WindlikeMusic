2a34a4a1969d023b04de0f82f704ba2fa47f4d42
windlike <838596151@qq.com>
2018-04-01 14:33:08 +0800

    update

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,10 +1,10 @@
-var cacheStorageKey = 'minimal-pwa-1';
+var cacheStorageKey = 'minimal-pwa-2';
 
 var cacheList = [
-  '/',
-  "index.html",
-  "app.js",
-  "icon.png"
+  './',
+  "./index.html",
+  "./app.js",
+  "./icon.png"
 ];
 
 self.addEventListener('install', e => {
@@ -12,5 +12,37 @@ self.addEventListener('install', e => {
     caches.open(cacheStorageKey)
     .then(cache => cache.addAll(cacheList))
     .then(() => self.skipWaiting())
+  )
+})
+
+self.addEventListener('activate', function(e) {
+  console.log('Activate event')
+  e.waitUntil(
+    Promise.all(
+      caches.keys().then(cacheNames => {
+        return cacheNames.map(name => {
+          if (name !== cacheStorageKey) {
+            return caches.delete(name)
+          }
+        })
+      })
+    ).then(() => {
+      console.log('Clients claims.')
+      return self.clients.claim()
+    })
+  )
+})
+
+self.addEventListener('fetch', function(e) {
+  // console.log('Fetch event:', e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      if (response != null) {
+        console.log('Using cache for:', e.request.url)
+        return response
+      }
+      console.log('Fallback to fetch:', e.request.url)
+      return fetch(e.request.url)
+    })
   )
 })
