5152ae91a72514ce76965f845e14dc43136c3886
windlike <838596151@qq.com>
2018-04-01 14:52:48 +0800

    update

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,15 +2,52 @@ var cacheStorageKey = 'minimal-pwa-2';
 
 var cacheList = [
   './',
-  "./index.html",
-  "./app.js",
+  "index.html",
+  "app.js",
   "./icon.png"
 ];
 
-self.addEventListener('install', e => {
+self.addEventListener('install', function(e) {
+  console.log('Cache event!')
   e.waitUntil(
-    caches.open(cacheStorageKey)
-    .then(cache => cache.addAll(cacheList))
-    .then(() => self.skipWaiting())
+    caches.open(cacheStorageKey).then(function(cache) {
+      console.log('Adding to Cache:', cacheList)
+      return cache.addAll(cacheList)
+    }).then(function() {
+      console.log('Skip waiting!')
+      return self.skipWaiting()
+    })
+  )
+})
+
+self.addEventListener('activate', function(e) {
+  console.log('Activate event')
+  e.waitUntil(
+    Promise.all(
+      caches.keys().then(cacheNames => {
+        return cacheNames.map(name => {
+          if (name !== cacheStorageKey) {
+            return caches.delete(name)
+          }
+        })
+      })
+    ).then(() => {
+      console.log('Clients claims.')
+      return self.clients.claim()
+    })
+  )
+})
+
+self.addEventListener('fetch', function(e) {
+  // console.log('Fetch event:', e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      if (response != null) {
+        console.log('Using cache for:', e.request.url)
+        return response
+      }
+      console.log('Fallback to fetch:', e.request.url)
+      return fetch(e.request.url)
+    })
   )
 })
